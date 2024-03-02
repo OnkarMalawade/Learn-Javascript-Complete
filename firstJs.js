62be0111f1aabe1a08fd126dfd52033d3227b533
OnkarMalawade <132775903+OnkarMalawade@users.noreply.github.com>
2024-03-02 11:38:29 +0000

    Last Best changes

--- firstJs.js
+++ firstJs.js
@@ -1,1 +1,30 @@
 console.log("Hello World");
+
+let scrore = prompt ("enter score ") ;
+
+console.log(score); 
+
+const std= {
+Fname : "Onkar", 
+roll : "35"
+};
+
+console.log(std[Fname]) ; // object
+
+typeof std ;
+
+let str = "Hello" ;
+for(let i of str){
+console.log(i);
+} // Every character print in the string 
+
+for(let key in std){ // every key object print like Fname Onkar
+  console.log(key, std[key]);
+}
+
+let mark = [97,67,32,48,99,96,86];
+// Rich function context
+let newArr = marks.filter((val)=>{
+ return val > 90;
+});
+console.log(newArr);
